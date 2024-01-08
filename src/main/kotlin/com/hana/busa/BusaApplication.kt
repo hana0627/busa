a81e31baa9098bede074d544d7ff65aa5a17dcf3
hana6270 <hanana9506@naver.com>
2024-01-09 00:02:11 +0900

    Initial commit

---
+++ src/main/kotlin/com/hana/busa/BusaApplication.kt
@@ -0,0 +1,11 @@
+package com.hana.busa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BusaApplication
+
+fun main(args: Array<String>) {
+    runApplication<BusaApplication>(*args)
+}
